99df747ceeb8b8b0513b1076ede57096e389c8ae
acozy03 <acozy03@gmail.com>
2025-03-17 12:31:32 -0400

    nodemailer update

--- backend/api/register.js
+++ backend/api/register.js
@@ -6,7 +6,6 @@ const client = new MongoClient(process.env.MONGODB_URL, { useNewUrlParser: true,
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
