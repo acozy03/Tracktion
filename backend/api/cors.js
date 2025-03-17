3006e8ef3270e82de470a1f848a37bc4bba1c87a
acozy03 <acozy03@gmail.com>
2025-03-17 13:45:25 -0400

    vercel limit :(

--- backend/api/cors.js
+++ backend/api/cors.js
@@ -1,6 +1,6 @@
 const setCorsHeaders = (res) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://tracktion-jade.vercel.app');  // Allow only your frontend domain
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');  // Allowed methods
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');  // Allowed methods
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allowed headers
     res.setHeader('Access-Control-Allow-Credentials', 'true');  // If you're using cookies
   };
