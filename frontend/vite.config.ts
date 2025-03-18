e002fce98c0e92146de499818c4a972a4266e1ac
acozy03 <acozy03@gmail.com>
2025-03-18 17:14:05 -0400

    idc

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,13 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-  base: './'
+  base: "./",  // ✅ Forces Vite to use relative paths
 });
