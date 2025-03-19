692e845b754a84b7238dacc7ac5fb89cf2fd9421
acozy03 <acozy03@gmail.com>
2025-03-18 20:42:01 -0400

    i really hungry

--- backend/api/habits/[habitId]/delete.js
+++ backend/api/habits/[habitId]/delete.js
@@ -1,4 +1,6 @@
-const { MongoClient, ObjectId } = require('mongodb');
+// api/habits/delete.js
+
+const { MongoClient, ObjectHabitId } = require('mongodb');
 const client = new MongoClient(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 const setCorsHeaders = require('../../cors');
 
@@ -10,19 +12,16 @@ module.exports = async (req, res) => {
   }
 
   if (req.method === 'DELETE') {
-    const { id } = req.query;
-
-    if (!id) {
-      console.error("Delete request received without ID.");
-      return res.status(400).json({ error: 'ID is required' });
+    const { habitId } = req.query;
+    console.log('Received DELETE request with HABITID:', habitId);
+    if (!habitId) {
+      return res.status(400).json({ error: 'HABITID is required' });
     }
 
     try {
       await client.connect();
       const db = client.db('LargeProject');
-      console.log("Received DELETE request with ID:", id);
-
-      const result = await db.collection('habits').deleteOne({ _id: new ObjectId(id) });
+      const result = await db.collection('habits').deleteOne({ _habitId: new ObjectHabitId(habitId) });
 
       if (result.deletedCount === 0) {
         return res.status(404).json({ error: 'Habit not found' });
@@ -30,7 +29,6 @@ module.exports = async (req, res) => {
 
       res.status(200).json({ message: 'Habit deleted successfully' });
     } catch (error) {
-      console.error('Error deleting habit:', error);
       res.status(500).json({ error: 'An error occurred deleting the habit' });
     } finally {
       await client.close();
