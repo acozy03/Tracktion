481dbd00841d7cb5dc19eb0729144f62ba976ea1
acozy03 <acozy03@gmail.com>
2025-03-18 21:10:59 -0400

    fjkdslafjdsjfdajlfdajfadjkl;fdajlfkadjdf

--- backend/api/habits/[habitId]/delete.js
+++ backend/api/habits/[habitId]/delete.js
@@ -1,6 +1,6 @@
 // api/habits/delete.js
 
-const { MongoClient, ObjectHabitId } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const client = new MongoClient(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 const setCorsHeaders = require('../../cors');
 
@@ -22,9 +22,9 @@ module.exports = async (req, res) => {
       await client.connect();
       const db = client.db('LargeProject');
       console.log("reached 1");
-      const test = await db.collection('habits').findOne({ _id: new ObjectHabitId(habitId) });
+      const test = await db.collection('habits').findOne({ _id: new ObjectId(habitId) });
       console.log("reached 2 : ", test); 
-      const result = await db.collection('habits').deleteOne({ _id: new ObjectHabitId(habitId) });
+      const result = await db.collection('habits').deleteOne({ _id: new ObjectId(habitId) });
 
       if (result.deletedCount === 0) {
         return res.status(404).json({ error: 'Habit not found' });
